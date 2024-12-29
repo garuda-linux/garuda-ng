5d51c45bbc350197c479d5f13fc1ff09b68217ed
Filip Myslinski <filipmyslinski2006@gmail.com>
2024-12-29 15:03:03 +0100

    test(toast): added correct providers to message toast service test

--- core/src/lib/message-toast/message-toast.service.spec.ts
+++ core/src/lib/message-toast/message-toast.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MessageToastService } from './message-toast.service';
+import { provideGarudaNG } from '../config';
 
 describe('MessageToastService', () => {
   let service: MessageToastService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideGarudaNG()],
+    });
     service = TestBed.inject(MessageToastService);
   });
 
