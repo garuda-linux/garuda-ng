a0fc71b5b5359c6923928c9289ffdbc0b21e3900
Filip Myslinski <filipmyslinski2006@gmail.com>
2024-12-31 01:24:02 +0100

    feat(card): added product-showcase card

---
+++ core/src/lib/card/index.ts
@@ -0,0 +1,7 @@
+export * from './card.component';
+export * from './directives/card-action.directive';
+export * from './directives/card-feature-list.directive';
+export * from './directives/card-feature-list-pro.component';
+export * from './directives/card-feature-list-con.component';
+export * from './directives/card-thumbnail.directive';
+export * from './directives/card-title.directive';
