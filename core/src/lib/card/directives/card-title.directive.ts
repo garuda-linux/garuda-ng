11b48b3980ea234e1c296abdc0eca2ba763a593a
Filip Myslinski <filipmyslinski2006@gmail.com>
2025-02-02 13:59:28 +0100

    fix(card): made card mobile friendly
    fix #62

--- core/src/lib/card/directives/card-title.directive.ts
+++ core/src/lib/card/directives/card-title.directive.ts
@@ -3,7 +3,8 @@ import { Directive } from '@angular/core';
 @Directive({
   selector: '[garudaCardTitle]',
   host: {
-    class: 'garuda-card__card-title',
+    'class': 'garuda-card__card-title',
+    '[style.place-self]': '"center"',
   },
 })
 export class CardTitleDirective {}
